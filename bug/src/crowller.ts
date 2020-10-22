672a5cfe4f00b591e1412007212d7fedcc3c7d34
-g <hanqiao1024@163.com>
2020-10-22 16:33:47 +0800

    feat: 内容的抓取

--- bug/src/crowller.ts
+++ bug/src/crowller.ts
@@ -1,1 +1,20 @@
-console.log('hello world')
+// ts 直接去引用js 会出现问题需要一个翻译文件： @types/...
+// ts -> .d.ts ->js
+import superagent from 'superagent'
+
+class Crowller {
+  private secret = 'secretKey'
+  private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`
+  private rawHtml = ''
+
+  async getRawHtml() {
+    const result = await superagent.get(this.url)
+    this.rawHtml = result.text
+  }
+
+  constructor() {
+    this.getRawHtml()
+  }
+}
+
+const crowller = new Crowller()
