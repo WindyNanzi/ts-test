8c4efcadba4bcd40d4012ce4760b05b5612fd512
hanqiao <17720074859@163.com>
2020-10-23 22:45:27 +0800

    feat: basic

---
+++ ts-study/src/01-basic.ts
@@ -0,0 +1,16 @@
+namespace Basic {
+  const isLoading: boolean = false
+
+  const dec: number = 6
+  const hex: number = 0xf00d
+  const bin: number = 0b1010
+  const oct: number = 0o744
+
+  const book: string = '深入浅出 Typescript'
+
+  function say(): void {
+    console.log('wooo')
+  }
+
+  const sy1 = Symbol()
+}
