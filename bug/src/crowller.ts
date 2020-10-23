d59f602952c8daf481a3179b09aa58684fd06607
-g <hanqiao1024@163.com>
2020-10-23 08:25:32 +0800

    feat: 数据爬取

--- bug/src/crowller.ts
+++ bug/src/crowller.ts
@@ -1,19 +1,75 @@
 // ts 直接去引用js 会出现问题需要一个翻译文件： @types/...
 // ts -> .d.ts ->js
 import superagent from 'superagent'
+import cheerio from 'cheerio'
+import fs from 'fs'
+import path from 'path'
 
+interface Imovie {
+  title: string
+  grade: string
+}
+
+interface Content {
+  [propName: number]: Array<Imovie>
+}
 class Crowller {
-  private secret = 'secretKey'
-  private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`
+  private url = `https://movie.douban.com/`
   private rawHtml = ''
 
   async getRawHtml() {
     const result = await superagent.get(this.url)
-    this.rawHtml = result.text
+    return result.text
+  }
+
+  getMovieInfo(html: string) {
+    const $ = cheerio.load(html)
+    const movies: Array<Imovie> = []
+
+    $('.ui-slide-item ul').each((_, ele) => {
+      const movie: Imovie = {
+        title: $(ele).find('.title').text().trim(),
+        grade:
+          $(ele)
+            .find('.rating .subject-rate')
+            .text()
+            .trim() || '暂无评分',
+      }
+      movies.push(movie)
+    })
+
+    return movies
+  }
+
+  generateJsonContent(movies: Array<Imovie>) {
+    const filePath = path.resolve(
+      __dirname,
+      '../data/movies.json'
+    )
+    let fileContent: Content = {}
+    if (fs.existsSync(filePath)) {
+      fileContent = {
+        ...JSON.parse(fs.readFileSync(filePath, 'utf-8')),
+        [new Date().getTime()]: JSON.stringify(fileContent),
+      }
+    }
+    fileContent[new Date().getTime()] = movies
+    return fileContent
+  }
+
+  async initSpiderProcess() {
+    const filePath = path.resolve(
+      __dirname,
+      '../data/movies.json'
+    )
+    const html = await this.getRawHtml()
+    const movies = this.getMovieInfo(html)
+    const fileContent = this.generateJsonContent(movies)
+    fs.writeFileSync(filePath, JSON.stringify(fileContent))
   }
 
   constructor() {
-    this.getRawHtml()
+    this.initSpiderProcess()
   }
 }
 
