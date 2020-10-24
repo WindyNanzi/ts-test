e49d70a373b6a3b8e5249d1d5bcac6d9eb53cfc8
hanqiao <17720074859@163.com>
2020-10-24 10:17:11 +0800

    feat: 基础类型

--- ts-study/src/01-basic.ts
+++ ts-study/src/01-basic.ts
@@ -13,4 +13,32 @@ namespace Basic {
   }
 
   const sy1 = Symbol()
+
+  const tuple: [string, number] = ['wooo', 3]
+
+  enum Month {
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December,
+  }
+
+  function isSummer(month: Month) {
+    switch (month) {
+      case Month.June:
+      case Month.July:
+      case Month.August:
+        return true
+      default:
+        return false
+    }
+  }
 }
